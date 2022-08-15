06da0ee76f37d489bc02d9701c62eea37127c2b8
jackline chepngeno <chepngenojackline0@gmail.com>
2022-08-15 22:19:14 +0300

    Done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
 #write your code here
 
-def countdown
+def countdown(num)
+    i = num
+    while i>0
+        puts "#{i} SECOND(S)!"
+        i -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+puts countdown (10)
+
+def countdown_with_sleep(num_sec)
+    i = num_sec 
+    while i>0
+        puts "#{i} SECOND(S)!"
+        i -= 1
+        sleep 1
+    end
+    "HAPPY NEW YEAR!"
+end
+puts countdown_with_sleep (4)
